a6976cfc502df797d8881e976aae65384d2d400f
rootlittledev <qwentin1997@gmail.com>
2018-06-13 23:55:21 +0300

    More Comments

--- app/src/main/java/com/example/etheros/arduinobluetooth/MainActivity.java
+++ app/src/main/java/com/example/etheros/arduinobluetooth/MainActivity.java
@@ -19,57 +19,59 @@ import android.widget.Toast;
 
 import java.util.Set;
 
+//Створюємо клас MainActivity який наслідує клас AppCompatActivity
 public class MainActivity extends AppCompatActivity {
 
-    // textview for connection status
+    //Поля користувацького інтерфейсу
     TextView textConnectionStatus;
     ListView pairedListView;
 
-    //An EXTRA to take the device MAC to the next activity
+    //Змінна для МАС адреса
     public static String EXTRA_DEVICE_ADDRESS;
 
-    // Member fields
+    //Змінні для інформації про bluetooth адаптер
     private BluetoothAdapter mBtAdapter;
     private ArrayAdapter<String> mPairedDevicesArrayAdapter;
 
+    //Ініціалізація
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
 
+        //Привязуємо змінні з елементами інтерфейсу
         textConnectionStatus = (TextView) findViewById(R.id.connecting);
         textConnectionStatus.setTextSize(40);
 
 
-        // Initialize array adapter for paired devices
+        // Зчитуємо приєднані bluetooth пристрої
         mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
 
-        // Find and set up the ListView for paired devices
+        // Відображаємо список пристроїв
         pairedListView = (ListView) findViewById(R.id.paired_devices);
         pairedListView.setOnItemClickListener(mDeviceClickListener);
         pairedListView.setAdapter(mPairedDevicesArrayAdapter);
 
     }
 
+    //перевантажуємо метод onResume який буде виконуватись при відновленні роботи з додатком
     @Override
     public void onResume()
     {
         super.onResume();
-        //It is best to check BT status at onResume in case something has changed while app was paused etc
+        //Перевіряємо чи ввімкнений bluetooth
         checkBTState();
 
-        mPairedDevicesArrayAdapter.clear();// clears the array so items aren't duplicated when resuming from onPause
+        mPairedDevicesArrayAdapter.clear();
 
-        textConnectionStatus.setText(" "); //makes the textview blank
+        textConnectionStatus.setText(" ");
 
-        // Get the local Bluetooth adapter
+        // Знаходим адаптер bluetooth телефона
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        // Get a set of currently paired devices and append to pairedDevices list
         Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
 
-        // Add previously paired devices to the array
         if (pairedDevices.size() > 0) {
             findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);//make title viewable
             for (BluetoothDevice device : pairedDevices) {
@@ -80,8 +82,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //method to check if the device has Bluetooth and if it is on.
-    //Prompts the user to turn it on if it is off
+    //Метод для перевірки чи включений bluetooth
     private void checkBTState()
     {
         // Check device has Bluetooth and that it is turned on
@@ -91,24 +92,24 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else {
             if (!mBtAdapter.isEnabled()) {
-                //Prompt user to turn on Bluetooth
+                //Якщо не включений надсилаєм запит на ввімкнення
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 1);
             }
         }
     }
 
-    // Set up on-click listener for the listview
+    // Встановлюємо дію при нажатті кнопки
     private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener()
     {
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3)
         {
             textConnectionStatus.setText("Connecting...");
-            // Get the device MAC address, which is the last 17 chars in the View
+            // записуємо МАС адрес вибраного пристрою
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
 
-            // Make an intent to start next activity while taking an extra which is the MAC address.
+            // Запускаємо головне меню
             Intent i = new Intent(MainActivity.this, ArduinoMain.class);
             i.putExtra(EXTRA_DEVICE_ADDRESS, address);
             startActivity(i);
